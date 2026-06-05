cb9eb3ca65cde5d9d84402ff05424195724e8131
ipzeroline <zeroline@live.com>
2026-06-05 08:27:25 +0700

    blogv1

---
+++ content/articles/index.ts
@@ -0,0 +1,101 @@
+import article47 from "./roe-high-pbv-less-than-1-meaning.json";
+import article46 from "./stocks-vs-ssf-rmf-mutual-fund-difference.json";
+import article45 from "./defensive-vs-cyclical-stocks-thailand.json";
+import article0 from "./stock-portfolio-100000-baht-thailand.json";
+import article1 from "./tdex-worth-buying-vs-individual-stocks.json";
+import article2 from "./ebank-etf-vs-bank-stocks-individual.json";
+import article3 from "./bh-stock-worth-buying-2026.json";
+import article4 from "./mtc-stock-worth-buying-2026.json";
+import article5 from "./gulf-stock-worth-buying-2026.json";
+import article6 from "./mint-stock-worth-buying-2026.json";
+import article7 from "./ivl-stock-worth-buying-2026.json";
+import article8 from "./hmpro-stock-worth-buying-2026.json";
+import article9 from "./scc-stock-worth-buying-2026.json";
+import article10 from "./bjc-stock-worth-buying-2026.json";
+import article11 from "./tisco-stock-worth-buying.json";
+import article12 from "./kbank-stock-worth-buying.json";
+import article13 from "./pttep-stock-worth-buying.json";
+import article14 from "./bbl-dividend-stock-analysis.json";
+import article15 from "./advanc-long-term-investment.json";
+import article16 from "./how-to-invest-sp500-thailand.json";
+import article17 from "./how-to-invest-nasdaq-thailand.json";
+import article18 from "./how-to-buy-nasdaq-100-etf.json";
+import article19 from "./how-to-open-etf-investment-account.json";
+import article20 from "./bbl-stock-worth-buying-2026.json";
+import article21 from "./ktb-stock-worth-buying-2026.json";
+import article22 from "./scb-stock-worth-buying-2026.json";
+import article23 from "./true-stock-worth-buying-2026.json";
+import article24 from "./intuch-stock-worth-buying-2026.json";
+import article25 from "./ptt-stock-worth-buying-2026.json";
+import article26 from "./aot-stock-worth-buying-2026.json";
+import article27 from "./bdms-stock-worth-buying-2026.json";
+import article28 from "./bch-stock-worth-buying-2026.json";
+import article29 from "./cpn-stock-worth-buying-2026.json";
+import article30 from "./cpall-stock-worth-buying-2026.json";
+import article31 from "./or-stock-worth-buying-2026.json";
+import article32 from "./best-stocks-to-buy-thailand-2026.json";
+import article33 from "./high-dividend-stocks-thailand-2026.json";
+import article34 from "./monthly-dividend-stocks-thailand.json";
+import article35 from "./dividend-stocks-for-retirement-thailand.json";
+import article36 from "./bank-dividend-stocks-thailand.json";
+import article37 from "./energy-dividend-stocks-thailand.json";
+import article38 from "./reit-high-dividend-thailand.json";
+import article39 from "./what-is-fair-value-stock.json";
+import article40 from "./what-is-peg-ratio-stock.json";
+import article41 from "./how-to-start-investing-in-stocks-thailand.json";
+import article42 from "./invest-1000-baht-per-month-stocks.json";
+import article43 from "./stocks-vs-mutual-funds-difference.json";
+import article44 from "./best-day-to-buy-stocks-thailand.json";
+
+import type { BlogArticle } from "@/lib/blogArticles";
+
+export const articleData = [
+  article47,
+  article46,
+  article45,
+  article0,
+  article1,
+  article2,
+  article3,
+  article4,
+  article5,
+  article6,
+  article7,
+  article8,
+  article9,
+  article10,
+  article11,
+  article12,
+  article13,
+  article14,
+  article15,
+  article16,
+  article17,
+  article18,
+  article19,
+  article20,
+  article21,
+  article22,
+  article23,
+  article24,
+  article25,
+  article26,
+  article27,
+  article28,
+  article29,
+  article30,
+  article31,
+  article32,
+  article33,
+  article34,
+  article35,
+  article36,
+  article37,
+  article38,
+  article39,
+  article40,
+  article41,
+  article42,
+  article43,
+  article44
+] satisfies BlogArticle[];
